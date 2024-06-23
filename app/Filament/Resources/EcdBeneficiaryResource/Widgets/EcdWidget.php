d9bcfffc4cf7341da46d6efcfd9ae4c9bc258a7c
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-06-23 16:33:44 +0200

    added widget on ecd list

---
+++ app/Filament/Resources/EcdBeneficiaryResource/Widgets/EcdWidget.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\EcdBeneficiaryResource\Widgets;
+
+use App\Filament\Resources\EcdBeneficiaryResource\Pages\ListEcdBeneficiaries;
+use App\Models\EcdBeneficiary;
+use Filament\Widgets\Concerns\InteractsWithPageTable;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Flowframe\Trend\Trend;
+
+class EcdWidget extends BaseWidget
+{
+    use InteractsWithPageTable;
+
+    protected static ?string $pollingInterval = null;
+
+    protected function getTablePage(): string
+    {
+        return ListEcdBeneficiaries::class;
+    }
+
+    protected function getStats(): array
+    {
+
+        $beneficiaryData = Trend::model(EcdBeneficiary::class)
+            ->between(
+                start: now()->subYear(),
+                end: now(),
+            )
+            ->perYear()
+            ->count();
+
+        return [
+
+        ];
+    }
+}
