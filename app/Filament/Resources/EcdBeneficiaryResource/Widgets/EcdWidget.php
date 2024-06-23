c5227dbe302159cc2eeb40385c32d59055e863fe
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-06-23 16:36:32 +0200

    wip

--- app/Filament/Resources/EcdBeneficiaryResource/Widgets/EcdWidget.php
+++ app/Filament/Resources/EcdBeneficiaryResource/Widgets/EcdWidget.php
@@ -6,33 +6,29 @@ use App\Filament\Resources\EcdBeneficiaryResource\Pages\ListEcdBeneficiaries;
 use App\Models\EcdBeneficiary;
 use Filament\Widgets\Concerns\InteractsWithPageTable;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Flowframe\Trend\Trend;
 
-class EcdWidget extends BaseWidget
-{
-    use InteractsWithPageTable;
+class EcdWidget extends BaseWidget {
+	use InteractsWithPageTable;
 
-    protected static ?string $pollingInterval = null;
+	protected static ?string $pollingInterval = null;
 
-    protected function getTablePage(): string
-    {
-        return ListEcdBeneficiaries::class;
-    }
+	protected function getTablePage(): string {
+		return ListEcdBeneficiaries::class;
+	}
 
-    protected function getStats(): array
-    {
+	protected function getStats(): array {
 
-        $beneficiaryData = Trend::model(EcdBeneficiary::class)
-            ->between(
-                start: now()->subYear(),
-                end: now(),
-            )
-            ->perYear()
-            ->count();
+		$beneficiaryData = Trend::model(EcdBeneficiary::class)
+			->between(
+				start: now()->subYear(),
+				end: now(),
+			)
+			->perYear()
+			->count();
 
-        return [
+		return [
 
-        ];
-    }
+		];
+	}
 }
